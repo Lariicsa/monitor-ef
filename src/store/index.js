85290fa9b634db772347ee15e29caaf36ab0a000
Larissa <dejamepienso@gmail.com>
2019-10-25 19:43:08 -0500

    create login and is working

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,34 @@
-import Vue from 'vue';
-import Vuex from 'vuex';
-// Vuex modules
-import auth from './auth';
+import Vue from 'vue'
+import Vuex from 'vuex'
+var firebase = require("firebase/app");
 
-Vue.use(Vuex);
+Vue.use(Vuex)
 
-const store = new Vuex.Store({
-  modules: {
-    auth,
-  },
-});
-
-export default store;
+export default new Vuex.Store({
+    state:{
+        user: '',
+        error: ''
+    },
+    mutations: {
+        setUser(state,payload){
+            state.user = payload
+        },
+        setError(state,payload) {
+          state.error = payload
+        }
+    },
+    actions: {
+        createUser({commit}, payload){
+            firebase.auth().createUserWithEmailAndPassword(payload.email, payload.pass)
+            .then(res=>{
+                console.log(res.user.email);
+                console.log(res.user.uid);
+                commit('setUser', {email: res.user.email, uid: res.user.uid})
+            })
+            .catch(err=>{
+                console.log(err.message);
+                commit('setError', err.message)
+            })
+        }
+    }
+})
