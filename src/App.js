3a0323575d4fd2b6835e7031699a402a0f3fb104
Larissa <dejamepienso@gmail.com>
2019-12-05 16:35:05 -0600

    Create Routes

--- src/App.js
+++ src/App.js
@@ -1,72 +1,23 @@
-import React, { Component } from 'react'
-import Header from './components/Header';
+import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import DetailStudent from './components/DetailStudent';
 import AddStudent from './components/AddStudent';
 import ListStudents from './components/ListStudents';
+import EditStudent from './components/EditStudent'
 
-class App extends Component {
-  state = {
-    students: []
-  }
-
-  componentDidMount() {
-    const ListStudentsLS = localStorage.getItem('students')
-    if (ListStudentsLS) {
-      this.setState({
-        students: JSON.parse(ListStudentsLS)
-      })
-    }
-  }
-
-  componentDidUpdate() {
-    localStorage.setItem('students', JSON.stringify(this.state.students))
-  }
-
-  addNewStudent = data => {
-    const students = [...this.state.students, data]
-    console.log(data);
-
-    this.setState({
-      students
-    })
-  }
-
-  deleteStudent = id => {
-    const currentStudents = [...this.state.students]
-
-    const students = currentStudents.filter(student => student.id !== id)
-
-    this.setState({
-      students
-    })
-    console.log('delete');
-  }
-  render() {
-    return (
-      <>
-        <Header title='Monitor EF' />
-        <div className="App container">
-          <section className="section">
-            <div className="columns box is centered">
-              <div className="column is-12">
-                <AddStudent
-                  addNewStudent={this.addNewStudent}
-                />
-              </div>
-            </div>
-          </section>
-          <section className="section">
-            <div className="columns box is centered">
-              <div className="column is-12">
-                <ListStudents
-                  students={this.state.students}
-                  deleteStudent={this.deleteStudent} />
-              </div>
-            </div>
-          </section>
-        </div>
-      </>
-    )
-  }
+function App() {
+  return (
+    <div>
+      <Router>
+        <Switch>
+          <Route exact path="/students" component={ListStudents} />
+          <Route exact path="/students/new" component={AddStudent} />
+          <Route exact path="/students/detail/:id" component={DetailStudent} />
+          <Route exact path="/students/edit/:id" component={EditStudent} />
+        </Switch>
+      </Router>
+    </div>
+  )
 }
 
-export default App;
+export default App
