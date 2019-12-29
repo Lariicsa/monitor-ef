507a9e35534a3dca6321fdef8df439ed60b14db4
Larissa <dejamepienso@gmail.com>
2019-12-28 20:19:54 -0600

    Add student detail

--- src/students/DetailStudent.js
+++ src/students/DetailStudent.js
@@ -1,9 +1,59 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { compose } from 'redux'
+import { connect } from 'react-redux'
+import { firestoreConnect } from 'react-redux-firebase'
+import { Link } from 'react-router-dom'
 
-export default function DetailStudent() {
-    return (
-        <div>
-            Detail
-        </div>
-    )
+class DetailStudent extends Component {
+    state = {}
+    render() {
+        const { student } = this.props
+
+        if (!student) return <strong>Cargando ...</strong>
+
+        return (
+            <div className="columns is-centered">
+                <div className="column is-8 box">
+                    <div className="columns is-centered">
+                        <div className="column is-12">
+                            <h2 className="title is-2 has-text-centered">Detalle de Alumno</h2>
+                        </div>
+                    </div>
+                    <div className="columns is-centered">
+                        <div className="column">
+                            <h3 className="title is-3">{student.name} {student.lastname1} {student.lastname2}</h3>
+                            <hr />
+                        </div>
+                    </div>
+                    <div className="columns is-centered">
+                        <div className="column">
+                            <p>Grupo: <strong>{student.level} {student.group}</strong></p>
+                            <p>Edad: <strong>{student.age}</strong></p>
+                            <p>Género: <strong>{student.gender}</strong></p>
+                            <hr />
+                        </div>
+                    </div>
+                    <div className="columns is-centered">
+                        <div className="column">
+                            <h3 className="title is-3">Mediciones</h3>
+                             <strong>{student.mediciones[0]}</strong>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
+
+export default compose(
+    firestoreConnect(props => [
+        {
+            collection: 'alumnos',
+            storeAs: 'student',
+            doc: props.match.params.id
+        }
+    ]),
+    connect(({ firestore: { ordered } }, props) => ({
+        student: ordered.student && ordered.student[0]
+    }))
+)(DetailStudent)
